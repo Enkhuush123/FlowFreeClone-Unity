ee24229371388a05558506977bd86b478c27abdd
Enkhbayar <enhuushaq@gmail.com>
2026-05-04 13:26:22 +0800

    fixedlines

--- LineDrawer.cs
+++ LineDrawer.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 public class LineDrawer : MonoBehaviour
 {
     public Camera cam;
+    public GameObject linePrefab;
 
-    private bool isDrawing;
+    private bool isDrawing = false;
     private string currentColorId = "";
     private Color currentColor;
+
+    private Cell startCell;
     private Cell lastCell;
 
-    private Color gridColor = new Color(0.85f, 0.85f, 0.85f);
-
+    private LineRenderer currentLine;
     private List<Cell> currentPath = new List<Cell>();
+
     private Dictionary<string, List<Cell>> savedPaths = new Dictionary<string, List<Cell>>();
+    private Dictionary<string, LineRenderer> savedLines = new Dictionary<string, LineRenderer>();
 
     void Update()
     {
@@ -36,6 +40,8 @@ public class LineDrawer : MonoBehaviour
 
         currentColorId = cell.colorId;
         currentColor = cell.dotColor;
+        startCell = cell;
+        lastCell = cell;
 
         ClearPath(currentColorId);
 
@@ -43,9 +49,14 @@ public class LineDrawer : MonoBehaviour
         currentPath.Add(cell);
 
         cell.pathOwner = currentColorId;
-        Paint(cell, currentColor);
 
-        lastCell = cell;
+        GameObject lineObj = Instantiate(linePrefab);
+        currentLine = lineObj.GetComponent<LineRenderer>();
+        currentLine.startColor = currentColor;
+        currentLine.endColor = currentColor;
+        currentLine.positionCount = 1;
+        currentLine.SetPosition(0, cell.transform.position);
+
         isDrawing = true;
     }
 
@@ -74,7 +85,8 @@ public class LineDrawer : MonoBehaviour
         currentPath.Add(cell);
         cell.pathOwner = currentColorId;
 
-        Paint(cell, currentColor);
+        AddPointToLine(cell.transform.position);
+
         lastCell = cell;
     }
 
@@ -82,12 +94,117 @@ public class LineDrawer : MonoBehaviour
     {
         if (!isDrawing) return;
 
-        savedPaths[currentColorId] = new List<Cell>(currentPath);
+        bool completed =
+            lastCell != null &&
+            lastCell.hasDot &&
+            lastCell.colorId == currentColorId &&
+            lastCell != startCell;
+
+        if (completed)
+        {
+            savedPaths[currentColorId] = new List<Cell>(currentPath);
+            savedLines[currentColorId] = currentLine;
+        }
+        else
+        {
+            ClearCurrentDrawing();
+        }
 
         isDrawing = false;
         currentColorId = "";
+        startCell = null;
         lastCell = null;
         currentPath.Clear();
+        currentLine = null;
+    }
+
+    void ClearCurrentDrawing()
+    {
+        foreach (Cell cell in currentPath)
+        {
+            if (cell != null && cell.pathOwner == currentColorId)
+                cell.pathOwner = "";
+        }
+
+        if (currentLine != null)
+            Destroy(currentLine.gameObject);
+    }
+
+    void AddPointToLine(Vector3 pos)
+    {
+        currentLine.positionCount++;
+        currentLine.SetPosition(currentLine.positionCount - 1, pos);
+    }
+
+    void RebuildCurrentLine()
+    {
+        if (currentLine == null) return;
+
+        currentLine.positionCount = currentPath.Count;
+
+        for (int i = 0; i < currentPath.Count; i++)
+        {
+            currentLine.SetPosition(i, currentPath[i].transform.position);
+        }
+    }
+
+    void UndoTo(Cell target)
+    {
+        int index = currentPath.IndexOf(target);
+
+        for (int i = currentPath.Count - 1; i > index; i--)
+        {
+            Cell cell = currentPath[i];
+
+            if (cell.pathOwner == currentColorId)
+                cell.pathOwner = "";
+
+            currentPath.RemoveAt(i);
+        }
+
+        lastCell = target;
+        RebuildCurrentLine();
+    }
+
+    public void ClearPath(string id)
+    {
+        if (savedPaths.ContainsKey(id))
+        {
+            foreach (Cell cell in savedPaths[id])
+            {
+                if (cell != null && cell.pathOwner == id)
+                    cell.pathOwner = "";
+            }
+
+            savedPaths[id].Clear();
+        }
+
+        if (savedLines.ContainsKey(id))
+        {
+            if (savedLines[id] != null)
+                Destroy(savedLines[id].gameObject);
+
+            savedLines.Remove(id);
+        }
+    }
+
+    public void ClearAllPaths()
+    {
+        foreach (string id in new List<string>(savedLines.Keys))
+        {
+            if (savedLines[id] != null)
+                Destroy(savedLines[id].gameObject);
+        }
+
+        savedLines.Clear();
+        savedPaths.Clear();
+
+        if (currentLine != null)
+            Destroy(currentLine.gameObject);
+
+        currentPath.Clear();
+        isDrawing = false;
+        currentColorId = "";
     }
 
     Cell GetCellUnderMouse()
@@ -107,71 +224,7 @@ public class LineDrawer : MonoBehaviour
 
     bool IsAdjacent(Cell a, Cell b)
     {
-        int distance = Mathf.Abs(a.row - b.row) + Mathf.Abs(a.col - b.col);
-        return distance == 1;
-    }
-
-    void UndoTo(Cell target)
-    {
-        int index = currentPath.IndexOf(target);
-
-        for (int i = currentPath.Count - 1; i > index; i--)
-        {
-            Cell cell = currentPath[i];
-            cell.pathOwner = "";
-            ResetCell(cell);
-            currentPath.RemoveAt(i);
-        }
-
-        lastCell = target;
-    }
-
-    public void ClearPath(string id)
-    {
-        if (!savedPaths.ContainsKey(id)) return;
-
-        foreach (Cell cell in savedPaths[id])
-        {
-            cell.pathOwner = "";
-            ResetCell(cell);
-        }
-
-        savedPaths[id].Clear();
-    }
-
-    public void ClearAllPaths()
-    {
-        foreach (var path in savedPaths.Values)
-        {
-            foreach (Cell cell in path)
-            {
-                if (cell != null)
-                {
-                    cell.pathOwner = "";
-                    ResetCell(cell);
-                }
-            }
-        }
-
-        savedPaths.Clear();
-        currentPath.Clear();
-        isDrawing = false;
-        currentColorId = "";
-        lastCell = null;
-    }
-
-    void ResetCell(Cell cell)
-    {
-        if (cell.hasDot)
-            Paint(cell, cell.dotColor);
-        else
-            Paint(cell, gridColor);
-    }
-
-    void Paint(Cell cell, Color color)
-    {
-        SpriteRenderer sr = cell.GetComponent<SpriteRenderer>();
-        sr.color = color;
+        return Mathf.Abs(a.row - b.row) + Mathf.Abs(a.col - b.col) == 1;
     }
 
     public bool IsPathConnected(string id)
